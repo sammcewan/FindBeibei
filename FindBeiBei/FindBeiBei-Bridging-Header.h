5c50a6fce35506c9b4e798a33e821364bb66766d
Sam McEwan <me@sammcewan.co.nz>
2014-07-09 22:14:35 +1200

    Implementing Estimote SDK

---
+++ FindBeiBei/FindBeiBei-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  FindBeiBei-Bridging-Header.h
+//  FindBeiBei
+//
+//  Created by Sam McEwan on 9/07/14.
+//  Copyright (c) 2014 Sam McEwan. All rights reserved.
+//
+
+#import <ESTBeaconManager.h>
